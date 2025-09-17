ce9c28059e970fe457a23e236736aeca57c30acb
new2010 <mahmoud_scorpion@yahoo.com>
2025-09-18 01:11:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // إعدادات API للمتجر
 // بعد نشر Google Apps Script، ضع الرابط هنا
-const API_BASE = "YOUR_WEB_APP_URL_HERE";
+const API_BASE = "https://script.google.com/macros/s/AKfycbzFYDMcyMhKIvVxaipO5QBpe57e-0FRqDTqSGDTkOfYCpqNx6r9w3A1qP5JLgLZKPWO/exec";
 
 // ملاحظة: لا تضع ADMIN_KEY هنا أبداً
 // سيتم إدخال ADMIN_KEY يدوياً في لوحة الأدمن عند تسجيل الدخول
